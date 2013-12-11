d217bbd8b63c2bbe26566ba13f2773761e86cf0f
MasahiroNanke <masahironanke@gmail.com>
2013-12-11 10:53:31 +0900

    リファクタリング（name変更）

--- src/com/example/musicgameanimation2/MainActivity.java
+++ src/com/example/musicgameanimation2/MainActivity.java
@@ -1,15 +1,48 @@
-package com.example.musicgameanimation2;
+/*http://ichitcltk.hustle.ne.jp/gudon/modules/pico_rd/index.php?content_id=113*/
+/*http://youinfo.sitemix.jp/uncategorized/705.html
+*/package com.example.musicgameanimation2;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TableLayout;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TableRow;
+import android.view.View;
 
 public class MainActivity extends Activity {
+	
+	private final static int WC=TableLayout.LayoutParams.WRAP_CONTENT;
+	private final static int FP=TableLayout.LayoutParams.FILL_PARENT;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		TableLayout tableLayout = new TableLayout(this);
+		setContentView(tableLayout, new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
+		 
+		int index =1;
+		
+			TableRow[] tableRow = new TableRow[4];
+			for (int iRow = 0; iRow < 4; iRow++) {
+				tableRow[iRow] = new TableRow(this);
+				tableRow[iRow].setLayoutParams(new TableLayout.LayoutParams(FP,FP));
+				tableRow[iRow].setGravity(Gravity.CENTER);//’†‰›Šñ‚¹
+				tableLayout.addView(tableRow[iRow]); 
+				
+				ImageButton[] button_list = new ImageButton[4];
+				for(int i = 0; i < 4; i++){
+					button_list[i] = new ImageButton(this);
+					button_list[i].setId(index++);
+					button_list[i].setImageResource(R.drawable.musicgame1);
+					tableRow[iRow].addView(button_list[i]);
+				}
+			}
 	}
 
 	@Override
@@ -20,3 +53,4 @@ public class MainActivity extends Activity {
 	}
 
 }
+     
